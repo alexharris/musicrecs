f40e4f8546e0c1bb39fb605303d848ea48ec3a0e
alexharris <alexharris6@gmail.com>
2019-07-30 14:57:45 -0700

    deploy

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
     <span class="link" @click="clearFilters();filterYoutube()">youtube</span>&nbsp;
     <span class="link" @click="clearFilters();filterSpotify()">spotify</span>&nbsp;
     <span class="link" @click="clearFilters();filterBandcamp()">bandcamp</span>&nbsp;
-    <span class="link" @click="clearFilters();filterSoundcloud()">soundcloud</span>
+    <span class="link" @click="clearFilters();filterSoundcloud()">soundcloud</span>&nbsp;
+    <span class="link" @click="clearFilters();filterOther()">other</span>
     <br /><br />
     <hr />
     <br />
@@ -25,7 +26,11 @@
         
         <div v-else-if="message.type == 'bandcamp'">
           <a :href="message.url">{{message.url}}</a>
-        </div>         
+        </div>  
+
+        <div v-else-if="message.type == 'other'">
+          <a :href="message.url">{{message.url}}</a>
+        </div>                 
 
       <!-- {{message.type}} -->
       <p><small>Posted by {{message.user}} on {{message.createdTimestamp}}</small></p>
@@ -88,7 +93,16 @@ export default {
         this.messages = this.filteredMessages
         
       })
-    },           
+    },  
+    filterOther() {
+      return this.messages.filter((message) => {
+        if(message.type == 'other') {
+          this.filteredMessages.push(message)
+        }
+        this.messages = this.filteredMessages
+        
+      })
+    },               
     filterNone() {
       this.messages = this.allMessages
     }
@@ -113,90 +127,127 @@ export default {
       if (message.content === 'ping') {
         // Send "pong" to the same channel
         message.channel.send('pong1');
-      } 
-      else if (message.content.includes('youtube') === true) {
-        // message.channel.send('youtube')
-        var validUrlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-        var url = message.content.match(validUrlRegex)
-     
-        // Get youtube id from url
-        var youtubeId = url[0].split('v=')[1];
-        var ampersandPosition = youtubeId.indexOf('&');
-        if(ampersandPosition != -1) {
-          youtubeId = youtubeId.substring(0, ampersandPosition);
-        }
-
-        firebase.firestore().collection("messages").add({
-            message: message.content,
-            user: message.author.username,
-            createdTimestamp: message.createdTimestamp,
-            youtubeId: youtubeId,
-            type: 'youtube'
-        })
-      }
-      else if (message.content.includes('spotify') === true) {
-        var validUrlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-        var url = message.content.match(validUrlRegex)
-  
-        // Get spotify id from url
-        var spotifyId = url[0].substring(url[0].lastIndexOf('/') + 1)
-
-        // check to see if this is an album
-        if(url[0].includes('album')) {
-          //then write the data
-          firebase.firestore().collection("messages").add({
-              message: message.content,
-              user: message.author.username,
-              createdTimestamp: message.createdTimestamp,
-              url: url[0],
-              type: 'spotify',
-              spotifyId: spotifyId,
-              spotifyTrackOrAlbum: 'album'
-          })          
-        // or if it is a track
-        } else if (url[0].includes('track')) {
-          // then write the data
-          firebase.firestore().collection("messages").add({
-              message: message.content,
-              user: message.author.username,
-              createdTimestamp: message.createdTimestamp,
-              url: url[0],
-              type: 'spotify',
-              spotifyId: spotifyId,
-              spotifyTrackOrAlbum: 'track'
-          })    
-        } else {
-          console.log('Not a valid Spotify link')
-        }
-
-
-
-      }    
-      else if (message.content.includes('soundcloud') === true) {
-        var validUrlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-        var url = message.content.match(validUrlRegex)
+        console.log('pong: ' + Date.now())
+      } else if (message.content.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/)) {
         
-        firebase.firestore().collection("messages").add({
-            message: message.content,
-            user: message.author.username,
-            createdTimestamp: message.createdTimestamp,
-            url: url[0],
-            type: 'soundcloud'
-        })
-      }   
-      else if (message.content.includes('bandcamp') === true) {
-        var validUrlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-        var url = message.content.match(validUrlRegex)
-        console.log(url)
+        message.react("🤖")
+        console.log('link received')
+        
+        if (message.content.includes('youtube') === true || message.content.includes('youtu.be') === true ) {
+          console.log('youtube received')
+          // message.channel.send('youtube')
+          var validUrlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+          var url = message.content.match(validUrlRegex)
 
-        firebase.firestore().collection("messages").add({
-            message: message.content,
-            user: message.author.username,
-            createdTimestamp: message.createdTimestamp,
-            url: url[0],
-            type: 'bandcamp'
-        })
-      }               
+          console.log(url)
+
+          // Get youtube id from url
+          
+          var youtubeId;
+
+          if (url[0].split('v=')[1] != undefined) {
+            youtubeId = url[0].split('v=')[1];
+            var ampersandPosition = youtubeId.indexOf('&');
+            if(ampersandPosition != -1) {
+              youtubeId = youtubeId.substring(0, ampersandPosition);
+            }
+          } else if (url[0].substring(url[0].lastIndexOf('/') + 1) != undefined) {
+            youtubeId = url[0].substring(url[0].lastIndexOf('/') + 1)
+          }
+
+
+          firebase.firestore().collection("messages").add({
+              message: message.content,
+              user: message.author.username,
+              createdTimestamp: message.createdTimestamp,
+              youtubeId: youtubeId,
+              type: 'youtube'
+          })
+        } else if (message.content.includes('spotify') === true) {
+
+          console.log('spotify received')
+
+          var validUrlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+          var url = message.content.match(validUrlRegex)
+  
+          // Get spotify id from url
+          var spotifyId = url[0].substring(url[0].lastIndexOf('/') + 1)
+
+          // check to see if this is an album
+          if(url[0].includes('album')) {
+            //then write the data
+            firebase.firestore().collection("messages").add({
+                message: message.content,
+                user: message.author.username,
+                createdTimestamp: message.createdTimestamp,
+                url: url[0],
+                type: 'spotify',
+                spotifyId: spotifyId,
+                spotifyTrackOrAlbum: 'album'
+            })          
+          // or if it is a track
+          } else if (url[0].includes('track')) {
+            // then write the data
+            firebase.firestore().collection("messages").add({
+                message: message.content,
+                user: message.author.username,
+                createdTimestamp: message.createdTimestamp,
+                url: url[0],
+                type: 'spotify',
+                spotifyId: spotifyId,
+                spotifyTrackOrAlbum: 'track'
+            })    
+          } else {
+            console.log('Not a valid Spotify link')
+          }
+        }    
+        else if (message.content.includes('soundcloud') === true) {
+
+          console.log('soundcloud received')
+
+          var validUrlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+          var url = message.content.match(validUrlRegex)
+          
+          firebase.firestore().collection("messages").add({
+              message: message.content,
+              user: message.author.username,
+              createdTimestamp: message.createdTimestamp,
+              url: url[0],
+              type: 'soundcloud'
+          })
+        }   
+        else if (message.content.includes('bandcamp') === true) {
+
+          console.log('bandcamp received')
+
+          var validUrlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+          var url = message.content.match(validUrlRegex)
+          console.log(url)
+
+          firebase.firestore().collection("messages").add({
+              message: message.content,
+              user: message.author.username,
+              createdTimestamp: message.createdTimestamp,
+              url: url[0],
+              type: 'bandcamp'
+          })
+        } else {
+          console.log('other link received')
+
+          var validUrlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+          var url = message.content.match(validUrlRegex)
+
+          firebase.firestore().collection("messages").add({
+              message: message.content,
+              user: message.author.username,
+              createdTimestamp: message.createdTimestamp,
+              url: url[0],
+              type: 'other'
+          })        
+        }  
+      }
+
+    
     });
 
     // Log our bot in using the token from https://discordapp.com/developers/applications/me
