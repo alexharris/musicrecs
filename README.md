c3d709c1549697a667b9c4be73c3bcf178c9571c
alexharris <alexharris6@gmail.com>
2019-07-31 18:48:13 -0700

    reverse order

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# musicrecs
+# linkBot
+
+linkBot is a discord bot that takes links posted to a discord channel and creates a simple webpage out of them. With a focus on music links, the bot sorts links into main the following categoriesL
+
+- Youtube
+- Spotify
+- Bandcamp
+- Gara
 
 Run locally with: `./node_modules/.bin/vue-cli-service serve`
 
-##deploy like this
+## deploy like this
 
 git subtree push --prefix dist origin gh-pages
 
--- src/App.vue
+++ src/App.vue
@@ -119,7 +119,7 @@ export default {
 
      // load  messages
 
-    firebase.firestore().collection("messages").orderBy("createdTimestamp").get().then((querySnapshot) => {
+    firebase.firestore().collection("messages").orderBy("createdTimestamp", "desc").get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
         // console.log(doc.id, " => ", doc.data().url);
